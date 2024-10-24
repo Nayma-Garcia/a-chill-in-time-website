669ec5c0399efb5addd83aa757e493fd7ae4b681
Nayma Garcia <nayma802@gmail.com>
2024-10-23 23:44:29 -0500

    quick ui change

--- src/App.js
+++ src/App.js
@@ -1,25 +1,26 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
+        <h1 className="title">A Chill In Time</h1>
+        <p className="description">
+          The game starts with a detective being pulled into his boss's office and sent undercover to the town of Scarlettville, a quaint village with a dark secret: a centuries-old cold case that needs solving.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <p className="description">
+          To crack the case, the detective must gain the trust of the townspeople, who aren't easy to talk to. By completing quests, doing favors, and giving gifts, you'll slowly uncover clues—but not everything will be given away so easily.
+        </p>
+        <p className="description">
+          The detective's cover job as a landscaper keeps him busy with daily tasks around the town, but he can also take on personal jobs from the townspeople. Explore, build relationships, and discover hidden secrets!
+        </p>
+        <p className="devlog-link">
+          Follow my devlogs to see the progress on the game.
+        </p>
       </header>
     </div>
   );
 }
 
 export default App;
+
