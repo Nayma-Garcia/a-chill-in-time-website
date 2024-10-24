30c7419c5627924de9b31f7517f9d482a399b340
Nayma Garcia <nayma802@gmail.com>
2024-10-24 02:07:02 -0500

    small ui changes

--- src/App.js
+++ src/App.js
@@ -4,16 +4,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1 className="title">A Chill In Time</h1>
-        <nav>
-          <ul className="navbar">
+        <nav className="navbar">
+          <ul>
             <li><a href="#about">About</a></li>
             <li><a href="#devlog">Devlog</a></li>
           </ul>
         </nav>
+        <h1 className="title">A Chill In Time</h1>
       </header>
 
       <section id="about" className="about-section">
+        <h2>About</h2>
         <p className="description">
           "A Chill In Time" follows a detective who is sent undercover to the town of Scarlettville, a quiet village with a hidden past. Tasked with solving a centuries-old cold case, the detective must gain the trust of the wary townsfolk. By completing quests, doing favors, and giving gifts, you’ll slowly uncover hidden clues—but not everything is revealed easily. As part of his cover, the detective works as a landscaper, completing daily tasks and taking on personal jobs from the townspeople. Explore, build relationships, and uncover hidden secrets in this mysterious adventure!
         </p>
